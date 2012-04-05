94a91a1823c8e22c0dda2c83aacb15a444431f04
Christian Semmler <csemmler.goe@googlemail.com>
2012-04-05 17:57:48 +0200

    Version update

--- vaultmp.h
+++ vaultmp.h
@@ -9,12 +9,11 @@
 #define THREAD_PRIORITY_NORMAL 1000
 #endif
 
-
 #define NO_CS_TIMEOUT
 
-#define DEDICATED_VERSION       "0.1a revision 148"
-#define MASTER_VERSION          "0.1a revision 145"
-#define CLIENT_VERSION          "0.1a revision 148"
+#define DEDICATED_VERSION       "0.1a snapshot \"Gary 1\""
+#define MASTER_VERSION          "0.1a snapshot \"Gary 1\""
+#define CLIENT_VERSION          "0.1a snapshot \"Gary 1\""
 
 static const unsigned int FALLOUT3_EN_VER17            =   0x00E59528;
 //static const unsigned int FALLOUT3_EN_VER17_STEAM      =
