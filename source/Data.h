4e81e0764f018e5e85253ab7667d8f913488328f
Christian Semmler <csemmler.goe@googlemail.com>
2012-05-24 19:09:22 +0200

    Move constructors for new parameters

--- source/Data.h
+++ source/Data.h
@@ -115,8 +115,13 @@ namespace Data
 			const _Parameter* const_param = NULL;
 
 		public:
+			// move constructors to the rescue
+			// otherwise temporaries would be passed to the const ctors
+
 			Parameter(RawParameter& param) : param(new RawParameter(param)) {}
+			Parameter(RawParameter&& param) : param(new RawParameter(param)) {}
 			Parameter(FuncParameter& param) : param(new FuncParameter(param)) {}
+			Parameter(FuncParameter&& param) : param(new FuncParameter(param)) {}
 			Parameter(const RawParameter& param) : const_param(&param) {}
 			Parameter(const FuncParameter& param) : const_param(&param) {}
 
