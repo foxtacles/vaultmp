1f85d088ee2c5e9a0ebbfca6095cb855c2cca455
Christian Semmler <csemmler.goe@googlemail.com>
2012-09-07 00:56:50 +0200

    Add NPC data to database

---
+++ other/f3_npcs.php
@@ -0,0 +1,56 @@
+<?php
+
+function import($file,$dlc)
+{
+	global $db;
+	$f=file_get_contents($file);
+	if($f===FALSE)
+		die("Error loading $file");
+	$c=explode("\n",$f);
+	createTable("npcs");
+	foreach($c as $t)
+	{
+		list($id,$female,$race,$essential,$deathitem)=explode("|",$t);
+		
+		$id=hexdec($id);
+		$race=hexdec($race);
+		$deathitem=hexdec($deathitem);
+
+if ($id == 0)
+continue;
+
+                $prep = $db->prepare("insert into npcs (baseID,essential,female,race,deathitem,dlc) values (?, ?, ?, ?, ?, ?)");
+
+                if ($prep === FALSE) {
+			echo "Fail: " . $id;
+			continue;
+		}
+		$r = $prep->execute(array($id, $essential,$female,$race,$deathitem,$dlc));
+if (!$r) {
+$arr = $prep->errorInfo();
+echo $arr[2];
+}
+
+	}
+}
+
+
+function createTable($tb)
+{
+	global $db;
+	$db->exec("CREATE TABLE $tb (baseID integer, essential integer, female integer, race integer, deathitem integer, dlc integer)");
+}
+
+
+$db = new PDO('sqlite:fallout3.sqlite');
+if(!$db)
+{
+    die("Errore Sqlite");
+}
+
+import("data3/f3_main_npc.txt",0);
+import("data3/f3_tp_npc.txt",1);
+import("data3/f3_oa_npc.txt",2);
+import("data3/f3_bsn_npc.txt",3);
+import("data3/f3_pl_npc.txt",4);
+import("data3/f3_mz_npc.txt",5);
---
+++ other/fnv_npcs.php
@@ -0,0 +1,61 @@
+<?php
+
+function import($file,$dlc)
+{
+	global $db;
+	$f=file_get_contents($file);
+	if($f===FALSE)
+		die("Error loading $file");
+	$c=explode("\n",$f);
+	createTable("npcs");
+	foreach($c as $t)
+	{
+		list($id,$female,$race,$essential,$deathitem)=explode("|",$t);
+		
+		$id=hexdec($id);
+		$race=hexdec($race);
+		$deathitem=hexdec($deathitem);
+
+if ($id == 0)
+continue;
+
+                $prep = $db->prepare("insert into npcs (baseID,essential,female,race,deathitem,dlc) values (?, ?, ?, ?, ?, ?)");
+
+                if ($prep === FALSE) {
+			echo "Fail: " . $id;
+			continue;
+		}
+		$r = $prep->execute(array($id, $essential,$female,$race,$deathitem,$dlc));
+if (!$r) {
+$arr = $prep->errorInfo();
+echo $arr[2];
+}
+
+	}
+}
+
+
+function createTable($tb)
+{
+	global $db;
+	$db->exec("CREATE TABLE $tb (baseID integer, essential integer, female integer, race integer, deathitem integer, dlc integer)");
+}
+
+
+$db = new PDO('sqlite:newvegas.sqlite');
+if(!$db)
+{
+    die("Errore Sqlite");
+}
+
+import("data/fnv_main_npc.txt",0);
+import("data/fnv_dm_npc.txt",1);
+import("data/fnv_hh_npc.txt",2);
+import("data/fnv_ow_npc.txt",3);
+import("data/fnv_lr_npc.txt",4);
+import("data/fnv_GunRunnersArsenal_npc.txt",5);
+
+import("data/fnv_ClassicPack_npc.txt",6);
+import("data/fnv_CaravanPack_npc.txt",7);
+import("data/fnv_TribalPack_npc.txt",8);
+import("data/fnv_MercenaryPack_npc.txt",9);
