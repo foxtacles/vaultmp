26dc9fada75d93e600bb40de6740f671fea0d2f4
Christian Semmler <csemmler.goe@googlemail.com>
2012-12-24 07:10:56 +0100

    version update gary 2.5

--- source/vaultmp.h
+++ source/vaultmp.h
@@ -8,9 +8,9 @@
 #define THREAD_PRIORITY_NORMAL 1000
 #endif
 
-#define DEDICATED_VERSION "0.1a snapshot \"Gary 2.4\""
-#define MASTER_VERSION "0.1a snapshot \"Gary 2.4\""
-#define CLIENT_VERSION "0.1a snapshot \"Gary 2.4\""
+#define DEDICATED_VERSION "0.1a snapshot \"Gary 2.5\""
+#define MASTER_VERSION "0.1a snapshot \"Gary 2.5\""
+#define CLIENT_VERSION "0.1a snapshot \"Gary 2.5\""
 
 static const unsigned int FALLOUT3_EN_VER17            =   0x00E59528;
 //static const unsigned int FALLOUT3_EN_VER17_STEAM      =
