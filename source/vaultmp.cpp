ab2784053a11b3460d54af13d08b2af712645bc4
Christian Semmler <csemmler.goe@googlemail.com>
2012-12-26 21:48:14 +0100

    remove comment

--- source/vaultmp.cpp
+++ source/vaultmp.cpp
@@ -364,10 +364,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int)
 	{
 		fclose(filecheck);
 		Utils::GenerateChecksum("vaultmp.dll", checksum, checksum_real);
-/*
+
 		if (checksum_real != VAULTMP_DLL)
 		    return MessageBox(NULL, "vaultmp.dll is not up to date!", "Error", MB_OK | MB_ICONERROR);
-*/
 	}
 	else
 		return MessageBox(nullptr, "Could not find vaultmp.dll!", "Error", MB_OK | MB_ICONERROR);
