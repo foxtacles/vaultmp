34ff5ae34859ea8d440a814dcb3c0683de162876
Christian Semmler <csemmler.goe@googlemail.com>
2013-07-01 19:57:50 +0200

    Minor improvement

--- source/Interface.cpp
+++ source/Interface.cpp
@@ -337,15 +337,12 @@ void Interface::CommandThreadSend()
 							CommandParsed stream = API::Translate(cmd);
 
 							for (auto it = stream.begin(); it != stream.end() && !endThread; ++it)
-								while (!pipeServer->Send(it->get()) && !endThread)
+								while (!endThread && !pipeServer->Send(it->get()))
 								{
 									DWORD error = GetLastError();
 
 									if (error == ERROR_BROKEN_PIPE || error == ERROR_NO_DATA)
-									{
 										endThread = true;
-										break;
-									}
 								}
 						}
 					}
@@ -368,15 +365,12 @@ void Interface::CommandThreadSend()
 						CommandParsed stream = API::Translate(cmd, dynamic.second);
 
 						for (auto it = stream.begin(); it != stream.end() && !endThread; ++it)
-							while (!pipeServer->Send(it->get()) && !endThread)
+							while (!endThread && !pipeServer->Send(it->get()))
 							{
 								DWORD error = GetLastError();
 
 								if (error == ERROR_BROKEN_PIPE || error == ERROR_NO_DATA)
-								{
 									endThread = true;
-									break;
-								}
 							}
 					}
 
