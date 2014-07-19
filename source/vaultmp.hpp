2be93337c1b1c31197ab0ba87f8497f43089489f
Christian Semmler <mail@csemmler.com>
2014-07-19 16:38:52 +0200

    version bump

--- source/vaultmp.hpp
+++ source/vaultmp.hpp
@@ -8,9 +8,9 @@
 #define THREAD_PRIORITY_NORMAL 1000
 #endif
 
-#define DEDICATED_VERSION "0.1a snapshot \"Gary 2.9\""
-#define MASTER_VERSION "0.1a snapshot \"Gary 2.9\""
-#define CLIENT_VERSION "0.1a snapshot \"Gary 2.9\""
+#define DEDICATED_VERSION "0.1a snapshot \"Gary 2.10\""
+#define MASTER_VERSION "0.1a snapshot \"Gary 2.10\""
+#define CLIENT_VERSION "0.1a snapshot \"Gary 2.10\""
 
 static const unsigned int FALLOUT3_EN_VER17            =   0x00E59528;
 static const unsigned int FOSE_VER0122                 =   0x0004E1B5;
