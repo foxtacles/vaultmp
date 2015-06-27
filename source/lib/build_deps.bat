922e09fa37dc381983899fc3b00c0add003e19fe
Koncord <koncord@rwa.su>
2015-06-28 04:12:49 +0900

    Use RakNet submodule Add universal makefiles for dependencies

---
+++ source/lib/build_deps.bat
@@ -0,0 +1,5 @@
+mingw32-make -f amx_makefile
+mingw32-make -f iniparser_makefile
+mingw32-make -f raknet_makefile
+mingw32-make -f sqlite_makefile
+mingw32-make -f time_makefile
---
+++ source/lib/build_deps.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+make -f amx_makefile
+make -f iniparser_makefile
+make -f raknet_makefile
+make -f sqlite_makefile
+make -f time_makefile
